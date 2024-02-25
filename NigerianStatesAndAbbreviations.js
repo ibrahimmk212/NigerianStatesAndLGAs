6383ef081089a79498c9084b0da2dfdbb9c6423e
Ibrahim Musa Hassan (Kida) <57891059+ibrahimmk212@users.noreply.github.com>
2024-02-25 18:52:37 +0100

    Create NigerianStatesAndAbbreviations.js

---
+++ NigerianStatesAndAbbreviations.js
@@ -0,0 +1,150 @@
+const states =[
+  {
+    "state": "Abia",
+    "abbreviation": "AB"
+  },
+  {
+    "state": "Abuja Federal Capital Territory",
+    "abbreviation": "FC"
+  },
+  {
+    "state": "Adamawa",
+    "abbreviation": "AD"
+  },
+  {
+    "state": "Akwa Ibom",
+    "abbreviation": "AK"
+  },
+  {
+    "state": "Anambra",
+    "abbreviation": "AN"
+  },
+  {
+    "state": "Bauchi",
+    "abbreviation": "BA"
+  },
+  {
+    "state": "Bayelsa",
+    "abbreviation": "BY"
+  },
+  {
+    "state": "Benue",
+    "abbreviation": "BE"
+  },
+  {
+    "state": "Borno",
+    "abbreviation": "BO"
+  },
+  {
+    "state": "Cross River",
+    "abbreviation": "CR"
+  },
+  {
+    "state": "Delta",
+    "abbreviation": "DE"
+  },
+  {
+    "state": "Ebonyi",
+    "abbreviation": "EB"
+  },
+  {
+    "state": "Edo",
+    "abbreviation": "ED"
+  },
+  {
+    "state": "Ekiti",
+    "abbreviation": "EK"
+  },
+  {
+    "state": "Enugu",
+    "abbreviation": "EN"
+  },
+  {
+    "state": "Gombe",
+    "abbreviation": "GO"
+  },
+  {
+    "state": "Imo",
+    "abbreviation": "IM"
+  },
+  {
+    "state": "Jigawa",
+    "abbreviation": "JI"
+  },
+  {
+    "state": "Kaduna",
+    "abbreviation": "KD"
+  },
+  {
+    "state": "Kano",
+    "abbreviation": "KN"
+  },
+  {
+    "state": "Katsina",
+    "abbreviation": "KT"
+  },
+  {
+    "state": "Kebbi",
+    "abbreviation": "KE"
+  },
+  {
+    "state": "Kogi",
+    "abbreviation": "KO"
+  },
+  {
+    "state": "Kwara",
+    "abbreviation": "KW"
+  },
+  {
+    "state": "Lagos",
+    "abbreviation": "LA"
+  },
+  {
+    "state": "Nasarawa",
+    "abbreviation": "NA"
+  },
+  {
+    "state": "Niger",
+    "abbreviation": "NI"
+  },
+  {
+    "state": "Ogun",
+    "abbreviation": "OG"
+  },
+  {
+    "state": "Ondo",
+    "abbreviation": "ON"
+  },
+  {
+    "state": "Osun",
+    "abbreviation": "OS"
+  },
+  {
+    "state": "Oyo",
+    "abbreviation": "OY"
+  },
+  {
+    "state": "Plateau",
+    "abbreviation": "PL"
+  },
+  {
+    "state": "Rivers",
+    "abbreviation": "RI"
+  },
+  {
+    "state": "Sokoto",
+    "abbreviation": "SO"
+  },
+  {
+    "state": "Taraba",
+    "abbreviation": "TA"
+  },
+  {
+    "state": "Yobe",
+    "abbreviation": "YO"
+  },
+  {
+    "state": "Zamfara",
+    "abbreviation": "ZA"
+  }
+]
